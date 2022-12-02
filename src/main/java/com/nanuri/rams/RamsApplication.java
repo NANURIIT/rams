0f3174fee6771670347f8c806f590dec7125bdc2
hongsk <hongsk@host.docker.internal>
2022-12-02 10:26:56 +0900

    setting

--- src/main/java/com/nanuri/rams/RamsApplication.java
+++ src/main/java/com/nanuri/rams/RamsApplication.java
@@ -1,10 +1,19 @@
 package com.nanuri.rams;
 
+import java.util.TimeZone;
+
+import javax.annotation.PostConstruct;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class RamsApplication {
+	
+	@PostConstruct
+    public void config() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(RamsApplication.class, args);
