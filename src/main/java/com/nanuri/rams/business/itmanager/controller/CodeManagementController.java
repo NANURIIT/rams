23e9c8de1b28ce1c6dfc381dfc6996ad97e2e3da
hosub <eddie121@kakao.com>
2022-12-06 14:01:47 +0900

    [ADD] 사용자관리 화면 생성

--- src/main/java/com/nanuri/rams/business/itmanager/controller/CodeManagementController.java
+++ src/main/java/com/nanuri/rams/business/itmanager/controller/CodeManagementController.java
@@ -6,8 +6,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class CodeManagementController {
 	
-	@GetMapping(value = {"/codeManagement"})
+	@GetMapping(value = {"/AC01010S"})
     public String codeManagement() {
-        return "business/codeManagement";
+        return "business/AC01010S";
     }
+	
+    @GetMapping(value = {"/AC01110S"})
+    public String userManagement() {
+        return "business/AC01110S";   
+    }
+
 }
