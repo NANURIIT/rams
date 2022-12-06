977111e4f6616bfb5d9bd299676162d5364af6ca
nanuriit <nanuriit@DESKTOP-QORJP2S>
2022-12-06 14:55:48 +0900

    [add] 심사정보 페이지 추가

---
+++ src/main/java/com/nanuri/rams/business/examinfo/controller/AS03010SAPIController.java
@@ -0,0 +1,14 @@
+package com.nanuri.rams.business.examinfo.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RequiredArgsConstructor
+@RestController
+// 안건조회 및 배정 페이지 API Controller
+public class AS03010SAPIController {
+
+}
