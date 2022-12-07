75805ccdc91565172bbcfd13f4cf2d082cde362a
hosub <eddie121@kakao.com>
2022-12-07 15:15:14 +0900

    [ADD] 협의체 사전준비 > 안건정보 조회 팝업 화면 생성

---
+++ src/main/resources/static/js/business/AS04110S.js
@@ -0,0 +1,24 @@
+$(document).ready(function($) {
+	
+	setOpenModal();
+	
+});
+
+function setOpenModal(){
+	let Modal = document.getElementById('AS04120P');
+	let OpenModal = document.getElementById("open_modal");
+	let CloseModal = document.getElementsByClassName("modal_close")[0];
+	
+	OpenModal.onclick = function() {
+	    Modal.style.display = "block";
+	}
+	
+	CloseModal.onclick = function() {
+	    Modal.style.display = "none";
+	}
+	window.onclick = function(event) {
+	    if (event.target === Modal) {
+	        Modal.style.display = "none";
+	    }
+	}
+}
