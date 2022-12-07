e6a6bd8328a5ce1396abb39fceffc8793d589f88
hosub <eddie121@kakao.com>
2022-12-07 16:49:04 +0900

    [ADD] 협의체 결과 화면 생성

--- src/main/java/com/nanuri/rams/business/examconsultative/controller/examConsultativeController.java
+++ src/main/java/com/nanuri/rams/business/examconsultative/controller/examConsultativeController.java
@@ -14,10 +14,10 @@ public class examConsultativeController {
     public String preAdvance() {
         return "business/AS04110S";
     }
-    // @GetMapping(value = {"/AS04210S"})
-    // public String resultConsultative() {
-    //     return "business/AS04210S";
-    // }
+    @GetMapping(value = {"/AS04210S"})
+    public String resultConsultative() {
+        return "business/AS04210S";
+    }
     // @GetMapping(value = {"/AS04310S"})
     // public String MeetingMaterial() {
     //     return "business/AS04310S";
