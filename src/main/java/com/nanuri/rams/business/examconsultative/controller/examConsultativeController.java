1c110c5944832c19d1b1fafc8180ba3152643a82
hosub <eddie121@kakao.com>
2022-12-07 14:41:09 +0900

    [ADD] 협의체 사전준비 화면 생성

--- src/main/java/com/nanuri/rams/business/examconsultative/controller/examConsultativeController.java
+++ src/main/java/com/nanuri/rams/business/examconsultative/controller/examConsultativeController.java
@@ -7,7 +7,19 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class examConsultativeController {
     
     @GetMapping(value = {"/AS04010S"})
-    public String resultConsultative() {
+    public String referConsultative() {
         return "business/AS04010S";
     }
+    @GetMapping(value = {"/AS04110S"})
+    public String preAdvance() {
+        return "business/AS04110S";
+    }
+    // @GetMapping(value = {"/AS04210S"})
+    // public String resultConsultative() {
+    //     return "business/AS04210S";
+    // }
+    // @GetMapping(value = {"/AS04310S"})
+    // public String MeetingMaterial() {
+    //     return "business/AS04310S";
+    // }
 }
