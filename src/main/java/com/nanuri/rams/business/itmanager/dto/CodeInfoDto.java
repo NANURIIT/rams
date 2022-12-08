bbf2d3090ea6dda3863c5ec8b25957874d3b2327
hosub <eddie121@kakao.com>
2022-12-08 09:46:09 +0900

    [ADD] itManagement dto 작성

--- src/main/java/com/nanuri/rams/business/itmanager/dto/CodeInfoDto.java
+++ src/main/java/com/nanuri/rams/business/itmanager/dto/CodeInfoDto.java
@@ -1,5 +1,18 @@
 package com.nanuri.rams.business.itmanager.dto;
 
-public class CodeInfoDto {
+import com.nanuri.rams.com.dto.CommonDTO;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CodeInfoDto extends CommonDTO {
+    /* 공통코드정보 */
+    private String cmnsCdGrp;               /* 공통코드그룹 */
+    private String cdVlId;                  /* 코드값ID */
+    private String cdVlNm;                  /* 코드값명 */
+    private String rsltCdVl;                /* 결과코드값 */
+    private Integer cdSq;                   /* 코드일련번호 */
+    private String useF;                    /* 사용여부 */
 }
--- src/main/java/com/nanuri/rams/business/itmanager/dto/GroupCodeInfoDto.java
+++ src/main/java/com/nanuri/rams/business/itmanager/dto/GroupCodeInfoDto.java
@@ -1,5 +1,19 @@
 package com.nanuri.rams.business.itmanager.dto;
 
-public class GroupCodeInfoDto {
+import com.nanuri.rams.com.dto.CommonDTO;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class GroupCodeInfoDto extends CommonDTO{
+    /* 공통코드정보 */
+    private String cmnsCdGrp;               /* 공통코드그룹 */
+    private String cmnsCdNm;                /* 공통코드명 */
+    private String cmnsCdClsf;              /* 공통코드구분 */
+    private Integer cdLngth;                /* 코드길이 */
+    private String trnsfrmAftCdGrp;         /* 변환후코드그룹 */
+    private String cmnsCdGrpExpl;           /* 공통코드그룹설명 */
+    private String useF;                    /* 사용여부 */
 }
