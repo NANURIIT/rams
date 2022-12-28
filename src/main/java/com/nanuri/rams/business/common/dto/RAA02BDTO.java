2084ab3680d819955a08e64d707bec9e39a15f98
nanuriit <nanuriit@DESKTOP-QORJP2S>
2022-12-28 09:58:20 +0900

    [update] deal 정보 생성 작업중

---
+++ src/main/java/com/nanuri/rams/business/common/dto/RAA02BDTO.java
@@ -0,0 +1,25 @@
+package com.nanuri.rams.business.common.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+/* 심사안건평가관리 table(RAA02BDTO) DTO */
+public class RAA02BDTO {
+
+	private String ibDealNo;
+	private String riskInspctCcd;
+	private String lstCCaseCcd;
+	private String raDealCcd;
+	private String fstRgstPDprtCd;
+	private String raStdYrMM;
+	private String raDealSq;
+	private String riskRcgNo;
+	private String ibDealNm;
+	private String ownPEnd;
+	private String fstRgstDt;
+	private String uptPEno;
+	private String uptRsnCntnt;
+
+}
