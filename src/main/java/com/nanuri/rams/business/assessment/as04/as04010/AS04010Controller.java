448ec667a2d689b541d617c705b5df548d7017e0
hongsk <hongsk@host.docker.internal>
2023-01-09 13:28:03 +0900

    [update]AS04010패키지

---
+++ src/main/java/com/nanuri/rams/business/assessment/as04/as04010/AS04010Controller.java
@@ -0,0 +1,13 @@
+package com.nanuri.rams.business.assessment.as04.as04010;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RequiredArgsConstructor
+@RestController
+public class AS04010Controller {
+
+}
---
+++ src/main/java/com/nanuri/rams/business/assessment/as04/as04010/AS04010Service.java
@@ -0,0 +1,8 @@
+package com.nanuri.rams.business.assessment.as04.as04010;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public interface AS04010Service {
+
+}
